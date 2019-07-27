f6d28ff5a6bad37ff3420d0fb0b2caa762801efa
Bryan <bryan_stevens314@yahoo.com>
2019-07-27 14:02:13 -0700

    Feature: SQLite setup

---
+++ PetGo/Models/Model.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+//Annotations for key
+using Microsoft.EntityFrameworkCore;
+namespace PetGo.SQLite {
+  public class UserContext : DbContext {
+    public DbSet<User> Users { get; set; }
+    protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder) {
+      optionsBuilder.UseSqlite ("Data Source=db.db");
+    }
+  }
+  public class User {
+    [Key]
+    public int uId { get; set; }
+    public string name { get; set; }
+    public string email { get; set; }
+    public string password { get; set; }
+
+  }
+}

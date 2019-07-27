0449c99b8fa054886850af51f2d1c2a6c800cc70
Bryan <bryan_stevens314@yahoo.com>
2019-07-27 14:11:20 -0700

    Feature: SQLite funtional when starting app

--- PetGo/Program.cs
+++ PetGo/Program.cs
@@ -8,17 +8,30 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace PetGo
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
+namespace PetGo.SQLite {
+    public class Program {
+        public static void Main (string[] args) {
+            using (var db = new UserContext ()) {
+                db.Users.Add (new User {
+                    name = "Bryan Stevens",
+                        email = "bryan_stevens314@yahoo.com",
+                        password = "00000"
+                });
+                var count = db.SaveChanges ();
+                Console.WriteLine ("{0} records saved to database", count);
+
+                Console.WriteLine ();
+                Console.WriteLine ("All users in database:");
+                foreach (var user in db.Users) {
+                    Console.WriteLine (" - {0}", user.name);
+                }
+
+            }
+            CreateWebHostBuilder (args).Build ().Run ();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IWebHostBuilder CreateWebHostBuilder (string[] args) =>
+            WebHost.CreateDefaultBuilder (args)
+            .UseStartup<Startup> ();
     }
 }

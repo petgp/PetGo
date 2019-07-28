bf3ffd30b29d101186c09cc9fdb0c535c081ae32
Bryan <bryan_stevens314@yahoo.com>
2019-07-28 12:28:14 -0700

    Attempting deploy

--- PetGo/Startup.cs
+++ PetGo/Startup.cs
@@ -1,12 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using PetGo.SQLite;
 namespace PetGo {
     public class Startup {
         public Startup (IConfiguration configuration) {

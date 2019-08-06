7c0a18b525fbc8dcbc84323205ca5892aa85d104
Bryan <bryan_stevens314@yahoo.com>
2019-08-05 20:02:36 -0700

    UI: Basic sidebar layout, sidebar buttons navigate to corresponding routes

--- PetGo/ClientApp/src/app/user-display/user-display.component.ts
+++ PetGo/ClientApp/src/app/user-display/user-display.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-user-display',
   templateUrl: './user-display.component.html',
   styleUrls: ['./user-display.component.css']
 })
-export class UserDisplayComponent implements OnInit {
+export class UserDisplayComponent {
+  public users: Users[];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    http.get<Users[]>(baseUrl + 'api/users').subscribe(result => {
+      console.log('HELLO RESULTS', result)
+      this.users = result;
+    }, error => console.error(error));
   }
 
+}
+interface Users {
+  Id: number;
+  Name: string;
+  Address: string;
+  City: string;
+  State: string;
+  Zip: number;
+  Country: string;
+  Email: string;
+  Password: string;
 }

5f8b6433b71b8d193970e9a9735363726584fd34
Bryan <bryan_stevens314@yahoo.com>
2019-08-28 12:24:10 -0700

    Issue #32 - Migrations in-op: commit 93368cf17b414a37f6a497cb95db168618105494 has functioning ef migrations

--- PetGo/Models/ListingModel.cs
+++ PetGo/Models/ListingModel.cs
@@ -1,21 +1,23 @@
 ﻿using System.ComponentModel.DataAnnotations;
-namespace PetGo.Models {
-    public class Listing {
-        [Key]
-        public int Id { get; set; }
-        public string Date { get; set; }
-        public string TimeoutDate { get; set; }
-        public int UserId { get; set; }
-        public int PetId { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public int ToUserId { get; set; }
+namespace PetGo.Models
+{
+  public class Listing
+  {
+    [Key]
+    public int Id { get; set; }
+    public string Date { get; set; }
+    public string TimeoutDate { get; set; }
+    public string UserId { get; set; }
+    public int PetId { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public int ToUserId { get; set; }
 
-    }
+  }
 
-    public class ListingWithPet
-    {
-        public Listing listing { get; set; }
-        public Pet pet { get; set; }
-    }
+  public class ListingWithPet
+  {
+    public Listing listing { get; set; }
+    public Pet pet { get; set; }
+  }
 }
--- PetGo/Models/PetModel.cs
+++ PetGo/Models/PetModel.cs
@@ -3,52 +3,54 @@ using System.ComponentModel.DataAnnotations;
 //Annotations for key
 using Microsoft.EntityFrameworkCore;
 
-namespace PetGo.Models {
+namespace PetGo.Models
+{
 
-    public class Pet {
-        [Key]
-        public int Id { get; set; }
-        public int Owner_id { get; set; }
-        public string Name { get; set; }
-        public string Type { get; set; }
-        public string Img_url { get; set; }
-        public string Breed { get; set; }
-        public int Age { get; set; }
-        public int Ownership_length { get; set; }
-        public Pet()
-        {
-
-        }
-        public Pet(PetWithBreed that)
-        {
-            this.Id = that.Id;
-            this.Owner_id = that.Owner_id;
-            this.Name = that.Name;
-            this.Type = that.Type;
-            this.Img_url = that.Img_url;
-            this.Breed = that.Breed;
-            this.Age = that.Age;
-            this.Ownership_length = that.Ownership_length;
-        }
-    }
-   
-
-
-    public class PetWithBreed : Pet
+  public class Pet
+  {
+    [Key]
+    public int Id { get; set; }
+    public string Owner_id { get; set; }
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public string Img_url { get; set; }
+    public string Breed { get; set; }
+    public int Age { get; set; }
+    public int Ownership_length { get; set; }
+    public Pet()
     {
-        public PetWithBreed() { }
-        public PetWithBreed(Pet pet, List<string> breeds)
-        {
-            this.Id = pet.Id;
-            this.Owner_id = pet.Owner_id;
-            this.Name = pet.Name;
-            this.Type = pet.Type;
-            this.Img_url = pet.Img_url;
-            this.Breed = pet.Breed;
-            this.Age = pet.Age;
-            this.Ownership_length = pet.Ownership_length;
-            this.breeds = breeds;
-        }
-        public List<string> breeds { get; set; }
+
     }
+    public Pet(PetWithBreed that)
+    {
+      this.Id = that.Id;
+      this.Owner_id = that.Owner_id;
+      this.Name = that.Name;
+      this.Type = that.Type;
+      this.Img_url = that.Img_url;
+      this.Breed = that.Breed;
+      this.Age = that.Age;
+      this.Ownership_length = that.Ownership_length;
+    }
+  }
+
+
+
+  public class PetWithBreed : Pet
+  {
+    public PetWithBreed() { }
+    public PetWithBreed(Pet pet, List<string> breeds)
+    {
+      this.Id = pet.Id;
+      this.Owner_id = pet.Owner_id;
+      this.Name = pet.Name;
+      this.Type = pet.Type;
+      this.Img_url = pet.Img_url;
+      this.Breed = pet.Breed;
+      this.Age = pet.Age;
+      this.Ownership_length = pet.Ownership_length;
+      this.breeds = breeds;
+    }
+    public List<string> breeds { get; set; }
+  }
 }
